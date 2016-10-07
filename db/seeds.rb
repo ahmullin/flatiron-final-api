45177d6ececa933593d4595b33802f85ee97fc59
lgants <lgants@gmail.com>
2016-10-07 15:38:43 -0400

    finalized updates to support vote functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,18 @@ end
   UserBook.create(user_id: rand(1..User.all.length), book_id: rand(1..Book.all.length))
 end
 
-30.times do
-	Chapter.create(title: "Chapter " + Faker::Hipster.word, author_id: rand(1..User.all.length), description: Faker::Hipster.sentence)
+60.times do
+	Chapter.create(title: "Chapter " + Faker::Hipster.word, author_id: rand(1..User.all.length), book_id: rand(1..Book.all.length), description: Faker::Hipster.sentence)
 end
 
-30.times do
+60.times do
   UserChapter.create(user_id: rand(1..User.all.length), chapter_id: rand(1..Chapter.all.length), vote_choice: rand(-1..1))
 end
 
-30.times do
-	Snippet.create(content: Faker::Hipster.paragraph, approved: false)
+120.times do
+	Snippet.create(content: Faker::Hipster.paragraph, chapter_id: rand(1..Chapter.all.length), approved: false)
 end
 
-30.times do
+120.times do
 	UserSnippet.create(user_id: rand(1..User.all.length), snippet_id: rand(1..Snippet.all.length), vote_choice: rand(-1..1))
 end
