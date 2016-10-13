bf3d3dd8e210dc456cba2acb4cf2aca465336c22
Alison <alisonhmullin@gmail.com>
2016-10-12 22:19:00 -0400

    add user snippet routes for voting

--- app/models/user_snippet.rb
+++ app/models/user_snippet.rb
@@ -1,4 +1,10 @@
 class UserSnippet < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :snippet, optional: true
+
+  def self.vote_count(snippet_id)
+    allVotes = UserSnippet.where(snippet_id: snippet_id)
+    allVotes.sum(:vote_choice)
+  end
+
 end
